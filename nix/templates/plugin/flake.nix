c36e6450f5ab6377d1996c6824f4c054b31e8045
Will Ruggiano <wmruggiano@gmail.com>
2023-04-14 19:17:17 -0600

    feat: add plugin template

--- nix/templates/plugin/flake.nix
+++ nix/templates/plugin/flake.nix
@@ -15,16 +15,32 @@
 
       systems = ["x86_64-linux"];
       perSystem = {pkgs, ...}: {
+        apps.init.program = pkgs.writeShellApplication {
+          name = "init";
+          runtimeInputs = with pkgs; [gnused ripgrep];
+          text = ''
+            mv lua/plugin.lua "lua/$1-firvish.lua"
+            rg -l REPLACE_ME | xargs -I{} sed -i s/REPLACE_ME/"$1"/g {}
+            git init && git add -A && git commit -am 'chore: initial commit'
+            echo 'use flake --impure' > .envrc && direnv allow
+            rm doc/.gitkeep
+            nix run .#generate-vimdoc
+            git add -A && git commit -a --amend --no-edit
+            # shellcheck disable=SC2016
+            echo '[firvish.nvim] template init done. You can remove `apps.init` from flake.nix now.'
+          '';
+        };
+
         apps.generate-vimdoc.program = pkgs.writeShellApplication {
           name = "generate-vimdoc";
           runtimeInputs = with pkgs; [lemmy-help];
           text = ''
-            lemmy-help -c lua/firvish.lua lua/firvish/extension.lua > doc/firvish.txt
+            lemmy-help -c lua/REPLACE_ME-firvish.lua > doc/REPLACE_ME-firvish.txt
           '';
         };
 
         devenv.shells.default = {
-          name = "firvish.nvim";
+          name = "REPLACE_ME.firvish";
           packages = with pkgs; [lemmy-help luajit zk];
           pre-commit.hooks = {
             alejandra.enable = true;
@@ -33,20 +49,8 @@
         };
 
         packages.default = pkgs.vimUtils.buildVimPluginFrom2Nix {
-          name = "firvish.nvim";
+          name = "REPLACE_ME.firvish";
           src = ./.;
-        };
-      };
-
-      flake = {
-        templates = {
-          default = {
-            path = ./nix/templates/plugin;
-            description = "Create a new firvish plugin";
-            welcomeText = ''
-              Run `nix run .#init -- PLUGIN_NAME` to get started
-            '';
-          };
         };
       };
     };
